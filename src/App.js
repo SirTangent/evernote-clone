1b4c6459722bfecd4188f4187ced489126fea5e3
Wyatt Phillips <datscigye@gmail.com>
2020-01-02 15:35:56 -0500

    Add quill box and debounce updater

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SidebarComponent from "./sidebar/sidebar";
+import EditorComponent from "./editor/editor";
 import './App.css';
 
 const firebase = require('firebase');
@@ -15,7 +17,12 @@ class App extends React.Component {
   }
 
   render() {
-    return(<div>Hello world</div>)
+    return(
+        <div className='app-container'>
+            <SidebarComponent></SidebarComponent>
+            <EditorComponent></EditorComponent>
+        </div>
+    );
   }
 
   componentDidMount = () => {
--- src/editor/editor.js
+++ src/editor/editor.js
@@ -5,13 +5,38 @@ import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 
+const UPDATE_TIME = 1500;
+
 class EditorComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            text: '',
+            title: '',
+            id: ''
+        };
     }
     render() {
-        return(<div>Hello from the editor</div>)
+
+        const {classes} = this.props;
+
+        return(
+            <div className={classes.editorContainer}>
+                <ReactQuill
+                    value={this.state.text}
+                    onChange={this.updateBody}>
+                </ReactQuill>
+            </div>
+        )
     }
+    updateBody = async (val) => {
+        await this.setState({text: val});
+        this.update();
+    };
+    update = debounce(() => {
+        console.log('UPDATE DATABASE');
+        // Come back later
+    }, UPDATE_TIME);
 }
 
 export default withStyles(styles)(EditorComponent);
