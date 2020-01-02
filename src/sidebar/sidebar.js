0fad9ba8b4b330100446281b8ff8258b9b8ef401
Wyatt Phillips <datscigye@gmail.com>
2020-01-02 16:09:24 -0500

    Add new note buttons

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -8,10 +8,58 @@ import SidebarItemComponent from '../sidebaritem/sidebarItem';
 class SidebarComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            addingNote: false,
+            title: null
+        }
     }
 
     render() {
-        return(<div>Hello from the sidebar</div>)
+
+        const {notes, classes, selectedNoteIndex} = this.props;
+
+        return(
+            <div className={classes.sidebarContainer}>
+                <Button
+                    onClick={this.newNoteBtnClick}
+                    className={classes.newNoteBtn}
+                >
+                    {
+                        this.state.addingNote ? 'Cancel' : 'New Note'
+                    }
+                </Button>
+                {
+                    this.state.addingNote ?
+                        <div>
+                            <input
+                                type='text'
+                                className={classes.newNoteInput}
+                                placeholder='Enter note title'
+                                onKeyUp={(e) => this.updateTitle(e.target.value)}
+                            >
+                            </input>
+                            <Button
+                                className={classes.newNoteSubmitBtn}
+                                onClick={this.newNote}
+                            >
+                                Submit Note
+                            </Button>
+                        </div> : null
+                }
+            </div>
+        );
+    }
+
+    newNote = () => {
+        console.log(this.state);
+    };
+
+    newNoteBtnClick = () => {
+        this.setState({title: null, addingNote: !this.state.addingNote})
+    };
+
+    updateTitle = (value) => {
+        this.setState({title: value});
     }
 }
 
