0fd57ccf587cf17c1ea73b48e634b869ab180419
Wyatt Phillips <datscigye@gmail.com>
2020-01-02 21:53:56 -0500

    Seperate firebase config to ignored file

--- src/index.js
+++ src/index.js
@@ -8,17 +8,7 @@ const firebase = require('firebase');
 require('firebase/firestore');
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCVnFthOIWbUuyWN1u60jBnpoRRwj_JY0U",
-    authDomain: "evernote-clone-81d64.firebaseapp.com",
-    databaseURL: "https://evernote-clone-81d64.firebaseio.com",
-    projectId: "evernote-clone-81d64",
-    storageBucket: "evernote-clone-81d64.appspot.com",
-    messagingSenderId: "268926128810",
-    appId: "1:268926128810:web:3cf0b03bde1a26d68f36d3",
-    measurementId: "G-ESSV38NZM2"
-};
-
+const firebaseConfig = require('./config/fb');
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
